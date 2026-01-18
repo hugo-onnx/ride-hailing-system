6a5fc544ac2c34889fb048418b091471d1d53431
hugo-onnx <hugonzalezhuerta@gmail.com>
2026-01-18 23:04:53 +0100

    feat: define feature and label columns for ETA prediction

---
+++ training/data/schema.py
@@ -0,0 +1,10 @@
+FEATURE_COLUMNS = [
+    "trip_distance_km",
+    "supply_demand_ratio",
+    "surge_pressure",
+    "deadhead_km_avg",
+    "available_drivers",
+    "ride_requests",
+]
+
+LABEL_COLUMN = "pickup_eta_seconds"
